b4eb110963b91367ab80f3652916a36d1a45cc46
Nikita <nromanchuk@xbsoftware.by>
2019-02-15 15:14:18 +0300

    added create_category route

---
+++ src/components/CreateCategoryRoute/CreateCategoryRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './CreateCategoryRoute.css';
+
+
+const CreateCategoryRoute = () => (
+	<React.Fragment>
+		<h2 className={styles.header}>Create New Post</h2>
+		<div className={styles.content}>
+			<form className="form-signin" />
+		</div>
+	</React.Fragment>
+);
+
+export default CreateCategoryRoute;
