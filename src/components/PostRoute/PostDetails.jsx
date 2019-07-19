cf9b1251f881b952e78700d518c92c0f04c31c11
Nikita <nromanchuk@xbsoftware.by>
2019-07-19 15:46:31 +0300

    moved post detailed view logic to another file

---
+++ src/components/PostRoute/PostDetails.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import FullSizeImage from '../MainRoute/FullSizeImage';
+import routes from '../../App/routeNames';
+
+import styles from './PostRoute.css';
+
+
+const Container = ({ match, selectedPost }) => {
+  const { postId } = match.params;
+  const exists = selectedPost || postId;
+
+	return (
+    exists 
+    ? <article className="col s12">
+        <Link to={routes.MAIN}>Back</Link>
+        <FullSizeImage src={selectedPost.img}/>
+
+        <span>{postId}</span>
+      </article>
+		: <Redirect to={routes.MAIN} />
+	);
+};
+
+Container.propTypes = {
+	match: PropTypes.shape({
+		params: PropTypes.shape({
+			postId: PropTypes.string.isRequired
+		}).isRequired
+	}).isRequired
+};
+ 
+export default Container;
--- src/components/PostRoute/PostRoute.jsx
+++ src/components/PostRoute/PostRoute.jsx
@@ -1,40 +1,17 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router-dom';
+
+import PostDetails from '../../containers/PostDetails';
 
 import routes from '../../App/routeNames';
 
-import styles from './PostRoute.css';
 
-
-const Container = ({ match }) => {
-	const { id } = match.params;
-
-	return (
-		<article className="col s12">
-			<div
-				className={styles.imgContainer}
-			/>
-			<Link to={routes.MAIN}>Back</Link>
-			<br />
-			<span>{id}</span>
-		</article>
-	);
-};
-
-const PostRoute = () => (
+const PostRoute = () => 
 	<Switch>
-		<Route exact path={`${routes.POSTS}/:id`} component={Container} />
+		<Route exact path={`${routes.POSTS}/:postId`} component={PostDetails} />
 	</Switch>
-);
+;
 
 
-Container.propTypes = {
-	match: PropTypes.shape({
-		params: PropTypes.shape({
-			id: PropTypes.string.isRequired
-		}).isRequired
-	}).isRequired
-};
 
 export default PostRoute;
